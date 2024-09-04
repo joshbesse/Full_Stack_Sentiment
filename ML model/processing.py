4f9750515a3a1fe48581081bfe74526f90d77fd0
joshbesse <joshua.besse3@gmail.com>
2024-09-03 22:29:18 -0400

    finished data preprocessing and data visualization

--- ML model/processing.py
+++ ML model/processing.py
@@ -32,8 +32,7 @@ def prepare_text(text):
 
     # join tokens back into a single string 
     cleaned_text = ' '.join(tokens)
-    return cleaned_text
-
+    return tokens, cleaned_text
 
 # download punkt tokenizer data
 nltk.download('punkt')
@@ -46,15 +45,23 @@ df = pd.read_csv('./ML model/sentiment_analysis.csv')
 df = df.drop_duplicates()
 
 # clean the input text 
-df['clean_text'] = df['text'].apply(prepare_text)
+df[['tokens', 'clean_text']] = df['text'].apply(prepare_text).apply(pd.Series)
+
+# save cleaned data as pickle file 
+df.to_pickle('./ML model/cleaned_data.pkl')
 
 # data visualization 
 # sentiment class distribution
-#plt.bar(df['sentiment'].value_counts().index, df['sentiment'].value_counts().values, color=['darkgray', 'lawngreen', 'red'])
-#plt.xlabel('Sentiment Classes')
-#plt.ylabel('Class Counts')
-#plt.title('Sentiment Class Distribution')
-#plt.show()
+plt.bar(df['sentiment'].value_counts().index, df['sentiment'].value_counts().values, color=['darkgray', 'lawngreen', 'red'])
+plt.xlabel('Sentiment Classes')
+plt.ylabel('Class Counts')
+plt.title('Sentiment Class Distribution')
+plt.show()
 
-# text length
-
+# text length distribution
+length_tokens = df['tokens'].apply(len)
+plt.hist(length_tokens, bins=15)
+plt.xlabel('Length of Text (in # of words)')
+plt.ylabel('Text Counts')
+plt.title('Text Length Distribution')
+plt.show()
