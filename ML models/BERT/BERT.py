7259569f3e16466d9512ee99fc6278c00a80652a
joshbesse <joshua.besse3@gmail.com>
2024-09-18 21:44:21 -0400

    updated models for new data

--- ML models/BERT/BERT.py
+++ ML models/BERT/BERT.py
@@ -1,10 +1,8 @@
 import pandas as pd 
-from sklearn.preprocessing import LabelEncoder
-from datasets import Dataset, ClassLabel
+from datasets import Dataset
 from transformers import BertTokenizer, BertForSequenceClassification, TrainingArguments, Trainer
 import torch
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-import joblib
 import seaborn as sns
 import matplotlib.pyplot as plt 
 
@@ -12,22 +10,13 @@ def tokenize_function(examples):
     return tokenizer(examples['clean_text'], padding='max_length', truncation=True, max_length=128)
 
 # load cleaned data
-df = pd.read_pickle('./ML models/cleaned_data.pkl')
-
-# sentiment encoding 
-label_encoder = LabelEncoder()
-df['label'] = label_encoder.fit_transform(df['sentiment'])
+df = pd.read_pickle('./ML models/cleaned_data2.pkl')
 
 # convert dataframe to hugging face dataset 
-dataset = Dataset.from_pandas(df[['clean_text', 'label']])
-
-# convert target column to ClassLabel type
-class_names = list(label_encoder.classes_)
-class_label = ClassLabel(names=class_names)
-dataset = dataset.cast_column('label', class_label)
+dataset = Dataset.from_pandas(df[['clean_text', 'category']])
 
 # split dataset into training and test sets
-train_test_split = dataset.train_test_split(test_size=0.2, stratify_by_column='label', seed=3)
+train_test_split = dataset.train_test_split(test_size=0.2, stratify_by_column='category', seed=3)
 train_dataset = train_test_split['train']
 test_dataset = train_test_split['test']
 
@@ -41,8 +30,8 @@ train_dataset = train_dataset.map(tokenize_function, batched=True)
 test_dataset = test_dataset.map(tokenize_function, batched=True)
 
 # set format for PyTorch
-train_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'label'])
-test_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'label'])
+train_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'category'])
+test_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'category'])
 
 # define training arguments
 training_args = TrainingArguments(
@@ -73,25 +62,24 @@ trainer.train()
 
 # examine results 
 eval_results = trainer.evaluate()
-print(f'Evaluation results: {eval_results}')
+print(f"Evaluation results: {eval_results}")
 
 # generate predictions for the test set 
 preds_output = trainer.predict(test_dataset)
 y_pred = torch.argmax(torch.tensor(preds_output.predictions), axis=1).numpy()
-y = test_dataset['label']
+y = test_dataset['category']
 
 # examine accuracy score, classification report, and confusion matrix
 accuracy = accuracy_score(y, y_pred)
-print(f'Accuracy Score: {accuracy}')
-report = classification_report(y, y_pred, target_names=label_encoder.classes_)
-print(f'Classification Report:\n {report}')
+print(f"Accuracy Score: {accuracy}")
+report = classification_report(y, y_pred)
+print(f"Classification Report:\n {report}")
 cm = confusion_matrix(y, y_pred)
-sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=label_encoder.classes_, yticklabels=label_encoder.classes_)
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
 plt.xlabel('Predicted')
 plt.ylabel('Actual')
 plt.show()
 
 # save tokenizer and model
 tokenizer.save_pretrained('./ML models/BERT/')
-joblib.dump(label_encoder, './ML models/BERT/label_encoder.pkl')
 model.save_pretrained('./ML models/BERT/')
--- ML models/Logistic Regression/logistic_reg.py
+++ ML models/Logistic Regression/logistic_reg.py
@@ -1,25 +1,24 @@
 import pandas as pd 
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import StratifiedKFold, cross_val_score, GridSearchCV, cross_val_predict
+from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import seaborn as sns 
 import matplotlib.pyplot as plt 
 import joblib
 
 # load cleaned data
-df = pd.read_pickle('./ML models/cleaned_data.pkl')
+df = pd.read_pickle('./ML models/cleaned_data2.pkl')
 
 # input text vectorization
 vectorizer = TfidfVectorizer(ngram_range=(1, 2))
 
-# sentiment encoding 
-label_encoder = LabelEncoder()  
-
 # extract features and targets
 X = vectorizer.fit_transform(df['clean_text']) # sparse matrix of TF-IDF values 
-y = label_encoder.fit_transform(df['sentiment']) # negative - 0 neutral - 1 positive - 2 
+y = df['category'] # -1: negative, 0: neutral, 1: positive
+
+# split data into training and test sets 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=3, stratify=y)
 
 # define model
 model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000)
@@ -31,35 +30,31 @@ param_grid = {
     'class_weight': [None, 'balanced']
 }
 
-# initialize stratified k-fold cross-validation and grid search 
-kf = StratifiedKFold(n_splits=5, shuffle=True, random_state=3)
-grid_search = GridSearchCV(estimator=model, param_grid=param_grid, scoring='f1_weighted', n_jobs=1, cv=kf, verbose=2)
+# hyperparameter tuning using grid search on training set  
+grid_search = GridSearchCV(estimator=model, param_grid=param_grid, scoring='f1_weighted', n_jobs=1, cv=5, verbose=2)
+grid_search.fit(X_train, y_train)
 
-# perform grid search
-grid_search.fit(X, y)
-
-# examine results 
+# examine grid search results
 print(f'Best Hyperparameters: {grid_search.best_params_}')
 print(f'Best Cross-Validation F1 Score: {grid_search.best_score_}')
-
-# save vectorizer, label encoder, and trained model
-joblib.dump(vectorizer, './ML models/Logistic Regression/tfidf_vectorizer.pkl')
-joblib.dump(label_encoder, './ML models/Logistic Regression/label_encoder.pkl')
-joblib.dump(grid_search.best_estimator_, './ML models/Logistic Regression/log_reg_model.pkl')
 
 # extract best model
 best_model = grid_search.best_estimator_
 
-# generate cross-validation predictions 
-y_pred = cross_val_predict(best_model, X, y, kf)
+# save vectorizer and trained model
+joblib.dump(vectorizer, './ML models/Logistic Regression/tfidf_vectorizer.pkl')
+joblib.dump(grid_search.best_estimator_, './ML models/Logistic Regression/log_reg_model.pkl')
+
+# evaluate model on test set 
+y_pred = best_model.predict(X_test)
 
 # examine accuracy score, classification report, and confusion matrix
-accuracy = accuracy_score(y, y_pred)
-print(f"Accuracy Score:", accuracy)
-report = classification_report(y, y_pred, target_names=label_encoder.classes_)
-print(f"Classification Report:\n", report)
-cm = confusion_matrix(y, y_pred)
-sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=label_encoder.classes_, yticklabels=label_encoder.classes_)
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy Score: {accuracy}")
+report = classification_report(y_test, y_pred)
+print(f"Classification Report:\n {report}")
+cm = confusion_matrix(y_test, y_pred)
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
 plt.xlabel('Predicted')
 plt.ylabel('Actual')
 plt.show()
